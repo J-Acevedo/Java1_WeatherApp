0c6e50497c4859347bc61fd692828150cc4aead4
Juan G. Acevedo-Roman <juan.acevedoroman@gmail.com>
2014-08-28 11:06:09 -0400

    TenDay Async done

--- WeatherApp/app/src/main/java/com/fullsail/juanacevedoroman/weatherapp/MyActivity.java
+++ WeatherApp/app/src/main/java/com/fullsail/juanacevedoroman/weatherapp/MyActivity.java
@@ -5,7 +5,13 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 
+import java.util.ArrayList;
+
+
 public class MyActivity extends Activity {
+
+    ArrayList<TenDayObject> tenDayData = new ArrayList<TenDayObject>();
+    TenDayForecast tenDayAsync;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,12 @@ public class MyActivity extends Activity {
         setContentView(R.layout.activity_my);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
+        // Ten Day ForeCast //
+
+        tenDayAsync = new TenDayForecast(this, tenDayData);
+        tenDayAsync.execute("http://api.wunderground.com/api/7ba4162762b10c05/forecast10day/q/FL/Orlando.json");
+
+        // Ten Day ForeCast //
 
     }
 
