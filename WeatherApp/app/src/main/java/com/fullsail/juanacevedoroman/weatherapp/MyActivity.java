10fec6f3e98617bb6cf44734a707e4ffa62b1054
Matthew Dutton <mdutton@fullsail.edu>
2014-08-28 10:58:35 -0400

    Async complete

--- WeatherApp/app/src/main/java/com/fullsail/juanacevedoroman/weatherapp/MyActivity.java
+++ WeatherApp/app/src/main/java/com/fullsail/juanacevedoroman/weatherapp/MyActivity.java
@@ -3,9 +3,13 @@ package com.fullsail.juanacevedoroman.weatherapp;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
 
 
 public class MyActivity extends Activity {
+
+
+    String todayAPI = "http://api.wunderground.com/api/7ba4162762b10c05/conditions/q/FL/Orlando.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,10 @@ public class MyActivity extends Activity {
         setContentView(R.layout.activity_my);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
+        TodayForecast today = new TodayForecast();
+        today.execute( todayAPI );
+
+        Log.d("DUTTON", "today");
 
     }
 
