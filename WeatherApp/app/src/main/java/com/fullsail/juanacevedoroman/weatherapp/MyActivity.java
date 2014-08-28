7fe07ff50ddd9e64d3acbc33fd82e69987bed48c
Matthew Dutton <mdutton@fullsail.edu>
2014-08-28 10:00:26 -0400

    set orientation

--- WeatherApp/app/src/main/java/com/fullsail/juanacevedoroman/weatherapp/MyActivity.java
+++ WeatherApp/app/src/main/java/com/fullsail/juanacevedoroman/weatherapp/MyActivity.java
@@ -1,9 +1,8 @@
 package com.fullsail.juanacevedoroman.weatherapp;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MyActivity extends Activity {
@@ -12,28 +11,11 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
 
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
 
 
